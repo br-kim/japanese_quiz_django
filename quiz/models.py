279dfd2e48bcd1030fa7765a4501c353465a17ef
idle947 <idle947@gmail.com>
2021-10-12 00:01:15 +0900

    add score model

--- quiz/models.py
+++ quiz/models.py
@@ -5,3 +5,9 @@ class User(models.Model):
     user_id = models.CharField(max_length=20)
     user_email = models.EmailField()
     user_password = models.CharField(max_length=20)
+
+
+class Score(models.Model):
+    user_id = models.CharField(max_length=20)
+    hiragana_score = models.CharField(max_length=20)
+    katakana_score = models.CharField(max_length=20)
