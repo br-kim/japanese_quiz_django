cc0e12dd047dd6abc8aa5166164a292a071467e9
idle947 <idle947@gmail.com>
2021-10-14 15:50:33 +0900

    create user app and move user model

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quiz.apps.QuizConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
--- quiz/models.py
+++ quiz/models.py
@@ -1,13 +1,7 @@
 from django.db import models
 
 
-class User(models.Model):
-    user_id = models.CharField(max_length=20)
-    user_email = models.EmailField()
-    user_password = models.CharField(max_length=20)
-
-
 class Score(models.Model):
     user_id = models.CharField(max_length=20)
-    hiragana_score = models.CharField(max_length=20)
-    katakana_score = models.CharField(max_length=20)
+    hiragana_score = models.JSONField(default=dict)
+    katakana_score = models.JSONField(default=dict)
