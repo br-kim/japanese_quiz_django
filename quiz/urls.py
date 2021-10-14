1fcf36da6659e2873659aca2ac2ffcaf9b5b8fa3
idle947 <idle947@gmail.com>
2021-10-14 15:41:35 +0900

    add quiz view

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,5 +6,8 @@ from quiz import views
 urlpatterns = [
     path('inf_quiz', views.inf_quiz),
     path('lim_quiz', views.lim_quiz),
-    path('scoreboard', None),
+    path('quiz_img', views.quiz_img),
+    path('quiz_imgs', views.quiz_imgs),
+    path('score_update', views.score_update)
+    # path('scoreboard', lambda x: return None),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
+import json
 
-from .models import User
+from django.shortcuts import render
+from django.http.response import HttpResponse, JsonResponse
+
+# from .models import Score
+import quiz.util
+from . import service
 
 
 def inf_quiz(request):
@@ -10,3 +14,23 @@ def inf_quiz(request):
 
 def lim_quiz(request):
     return render(request, "new_quiz.html")
+
+
+def quiz_img(request):
+    kind = request.GET.get("kind")
+    return JsonResponse({"path": quiz.util.get_gana_url(kind)})
+
+
+def quiz_imgs(request):
+    kind = request.GET.get("kind")
+    return JsonResponse({"order": quiz.util.get_gana_url_list(kind)})
+
+
+def score_update(request):
+    body = json.loads(request.body)
+    kind = body.get("kind")
+    character = body.get("character")
+    print(kind, character)
+    user_id = "admin"  # 나중에 유저 로그인 구현 후 변경
+    service.update_score(kind, character, user_id)
+    return HttpResponse(status=204)
