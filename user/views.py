c1456426c6ec083a182ba72c1a9b556f985040b6
idle947 <idle947@gmail.com>
2021-10-15 14:03:40 +0900

    add scoreboard, user profile view

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from django.http.response import HttpResponseRedirect
 
 from .form import UserRegistrationForm
 from .service import register_user
-from quiz.service import create_score
+from quiz.service import create_score, get_score
 
 
 def create_user_page(request):
@@ -30,3 +30,14 @@ def delete_user():
 
 def edit_user():
     return
+
+
+def user_profile(request):
+    return render(request, "user_profile.html")
+
+
+def user_scoreboard(request):
+    score = get_score(request.user)
+    return render(request, "scoreboard.html",
+                  context={"hiragana_score": score.get('hiragana_score'),
+                           "katakana_score": score.get('katakana_score')})
