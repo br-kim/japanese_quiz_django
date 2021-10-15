f0a2e4e8e20c8e4fab8545d5b1bdf76710e82f09
idle947 <idle947@gmail.com>
2021-10-15 11:56:47 +0900

    init user.js

---
+++ static/js/user.js
@@ -0,0 +1,9 @@
+// let userFunction = {
+//     createUser : function (){
+//         data =
+//         fetch("url",{
+//             method: "POST",
+//             body: JSON.stringify(data),
+//         })
+//     },
+// }
