0b7c5e35025d59e809d5392230868d9cf0da90b9
idle947 <idle947@gmail.com>
2021-10-18 22:20:26 +0900

    create community service

---
+++ community/service.py
@@ -0,0 +1,28 @@
+from django.core.paginator import Paginator
+
+from community import models
+
+
+def get_article_list():
+    return
+
+
+def get_article(article_id):
+    return
+
+
+def delete_article(article_id):
+    return
+
+
+def edit_article(article_id, contents):
+    return
+
+
+def get_comment(comment_id):
+    return
+
+
+def delete_comment(comment_id):
+    return
+
