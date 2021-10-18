cd6b28b1592b48fd19d1c2f52f063e25f49b963c
idle947 <idle947@gmail.com>
2021-10-18 20:00:31 +0900

    create community models

--- community/models.py
+++ community/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Articles(TimestampMixin, models.Model):
+    user_id = models.CharField(max_length=20)
+    title = models.TextField()
+    contents = models.TextField()
+
+
+class Comments(TimestampMixin, models.Model):
+    user_id = models.CharField(max_length=20)
+    contents = models.TextField()
+    article_id = models.IntegerField()
