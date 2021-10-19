f063b34078dc6820c79aeb0642257c37bdd3387d
idle947 <idle947@gmail.com>
2021-10-19 22:03:46 +0900

    edit community urls

--- community/urls.py
+++ community/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('write_page', views.write_page),
     path('write', views.write),
     path('edit_page', views.edit_page),
-    path('edit/<int:article_id>', views.edit),
-    path('delete/<int:article_id>', views.delete),
+    path('edit/<int:content_id>', views.edit),
+    path('delete/<int:content_id>', views.delete),
     path('article/<int:article_id>', views.get_article),
     path('article_page/<int:page_num>', views.get_article_page),
 ]
