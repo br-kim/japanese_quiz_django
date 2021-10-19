aabecd7692ccd9c05a22e1d57e8b6f23105dcbf0
idle947 <idle947@gmail.com>
2021-10-19 22:04:02 +0900

    implement community service

--- community/service.py
+++ community/service.py
@@ -4,33 +4,39 @@ from community import models
 
 
 def create_article(writer, title, contents):
-    return
+    return models.Articles.objects.create(user_id=writer, title=title, contents=contents)
 
 
 def create_comment(writer, contents):
-    return
+    return models.Comments.objects.create(user_id=writer, contents=contents)
 
 
 def get_article_list(page_num):
-    return
+    p = Paginator(models.Articles.objects.order_by("id").all(), 3)
+    return p.page(page_num).object_list
 
 
 def get_article(article_id):
-    return
+    return models.Articles.objects.get(id=article_id)
 
 
 def delete_article(article_id):
-    return
+    return models.Articles.objects.filter(id=article_id).delete()
 
 
-def edit_article(article_id, contents):
-    return
+def edit_article(article_id, title, contents):
+    article = models.Articles.objects.get(id=article_id)
+    if title:
+        article.title = title
+    if contents:
+        article.contents = contents
+    return article.save()
 
 
-def get_comment(comment_id):
-    return
+def get_comment(article_id):
+    return models.Comments.objects.filter(article_id=article_id)
 
 
 def delete_comment(comment_id):
-    return
+    return models.Comments.objects.filter(id=comment_id).delete()
 
