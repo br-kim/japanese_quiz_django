39dc019be9dc0714a762168e04100917e0a96047
br-kim <idle947@gmail.com>
2021-10-21 16:28:13 +0900

    add article urls

--- community/urls.py
+++ community/urls.py
@@ -4,7 +4,7 @@ from community import views
 
 
 urlpatterns = [
-    path('', views.main),
+    path('main', views.main),
     path('write_page', views.write_page),
     path('write', views.write),
     path('edit_page', views.edit_page),
@@ -12,5 +12,7 @@ urlpatterns = [
     path('delete/<int:content_id>', views.delete),
     path('article/<int:article_id>', views.get_article),
     path('article/write', views.write_page),
-    path('article_page/<int:page_num>', views.get_article_page),
+    path('article_list/<int:page_num>', views.get_article_list),
+    path('article/<int:article_id>/comment', views.get_comments),
+    path('article_page', views.article_page),
 ]
