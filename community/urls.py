2e11c4822062440f00bf2c61040ac3f8f9bfe433
br-kim <idle947@gmail.com>
2021-10-22 12:16:38 +0900

    edit delete urls

--- community/urls.py
+++ community/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('write', views.write),
     path('edit_page', views.edit_page),
     path('edit/<int:content_id>', views.edit),
-    path('delete/<int:content_id>', views.delete),
+    path('delete', views.delete),
     path('article/<int:article_id>', views.get_article),
     path('article/write', views.write_page),
     path('article_list/<int:page_num>', views.get_article_list),
