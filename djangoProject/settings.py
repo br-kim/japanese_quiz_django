802b8fd4f499c72521c10bf41fa5aed63d4947f8
idle947 <idle947@gmail.com>
2021-11-02 13:06:07 +0900

    edit static root dir setting

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = '/static_files/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
