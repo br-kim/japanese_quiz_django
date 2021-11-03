daff2e66211dfbd3863a1d109fd0f9734e07684a
br-kim <idle947@gmail.com>
2021-11-03 16:04:50 +0900

    using redis WIP

--- chatting/consumers.py
+++ chatting/consumers.py
@@ -6,12 +6,16 @@ import aioredis
 # from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-# redis_db = await aioredis.from_url()
 
-# redis_db = await aioredis.create_pool(os.environ.get("REDIS-DJANGO"))
+# redis_db = await aioredis.from_url()
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+
+    @property
+    async def get_connection_pool(self):
+        return await aioredis.create_pool(os.environ.get("REDIS_DJANGO"))
+
     async def connect(self):
         # self.room_name = self.scope
         self.room_group_name = 'chat_room'
@@ -19,10 +23,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+        # print(os.environ.get("REDIS_DJANGO"))
+        user_id = self.scope.get('user').username
 
+        # await redis_db.execute('set', 'test', '123')
+        # print(await redis_db.execute('get', 'test'))
+        pool = await self.get_connection_pool
+        await pool.execute('lpush', 'users', user_id)
+        result = await pool.execute('lrange', 'users', 0,-1)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': "chat_message",
+                'message': {
+                    'type': 'alert',
+                    'message': [i.decode() for i in result],
+                },
+            }
+        )
         await self.accept()
 
     async def disconnect(self, code):
+        user_id = self.scope.get('user').username
+        pool = await self.get_connection_pool
+        await pool.execute('lrem','users',user_id )
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
