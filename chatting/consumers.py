30cb6bded76e631a24ad418325ea6c13b9c3de90
idle947 <idle947@gmail.com>
2021-11-10 18:24:40 +0900

    disconnect connection when newly connected

--- chatting/consumers.py
+++ chatting/consumers.py
@@ -15,18 +15,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.room_group_name = 'chat_room'
+        user_id = self.scope.get('user').username
+        connection = await self.get_connection
+        print(user_id.encode(), await connection.lrange('users_list', 0, -1))
+        if user_id.encode() in await connection.lrange('users_list', 0, -1):
+            await self.disconnect(None)
+            await self.connect()
+            return
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         print("connected")
-        user_id = self.scope.get('user').username
 
-        connection = await self.get_connection
         await connection.lpush('users_list', user_id)
-
         result = await connection.lrange('users_list', 0, -1)
         print(result)
+
         await self.accept()
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -38,7 +44,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 },
             }
         )
-        print("send1")
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -47,7 +52,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
                             'message': "enter the chatting room."}
             }
         )
-        print("send2")
 
         await self.send(json.dumps({'type': 'user_id', 'message': user_id}))
         connection.close()
@@ -69,6 +73,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
         connection.close()
 
     async def receive(self, text_data=None, bytes_data=None):
