1763aa5b495051c548eee7748b0aebb40f25060f
idle947 <idle947@gmail.com>
2021-11-11 12:51:11 +0900

    add allow localhost

--- settings/settings_production.py
+++ settings/settings_production.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["limitless-plateau-99008.herokuapp.com"]
+ALLOWED_HOSTS = ["limitless-plateau-99008.herokuapp.com", "127.0.0.1"]
 
 CSRF_COOKIE_SECURE = True
 
