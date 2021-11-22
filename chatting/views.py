a737ef63f19d6a7bb8bbacadbb8166367ca88b4e
idle947 <idle947@gmail.com>
2021-11-22 21:03:55 +0900

    chatting main page require login

--- chatting/views.py
+++ chatting/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+ERROR_PAGE = "/user/need_login"
 
 
+@login_required(login_url=ERROR_PAGE)
 def main(request):
     return render(request, "chatting.html")
